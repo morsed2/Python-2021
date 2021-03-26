9e8c31eaef98b37da9f264b8603c556520093b78
Steve Morse <morsed2+gethub@gmail.com>
2021-03-25 22:16:56 -0700

    End of day backup 03-25-2021 up to lesson 10

---
+++ Lesson-08 PythonJournaling.py
@@ -0,0 +1,37 @@
+# name:				Python Journaling - #8
+# YouTube Link:		https://www.youtube.com/watch?v=OhA5ayySXxw&list=PLCC34OHNcOtrZTjsC5qtn_X9eRAmGmGOI&index=8
+#
+# Filename:  		'Lesson-08 PythonJournaling.py'
+#
+#  $ cd "/c/Users/Nick/PycharmProjects/AA Python Programming 2021"      to get to the directory
+#
+#            Git push commands
+# git add .
+# git commit -am "SuitableText"
+# git push
+# <password, tavr>
+
+
+# 
+#
+# 
+# 
+# 
+
+
+import os
+os.system("clear")
+
+print("Journaling, writing things down")
+print("Keep a journal book for each lanuage")
+print("Buy his book!")
+print("I already do this, he want ")
+print("It costs $27 and is avaiable from Amazon")
+
+
+
+
+
+
+
+
---
+++ Lesson-09 Strings.py
@@ -0,0 +1,46 @@
+# name:				Strings in Python - #9
+# YouTube Link:		https://www.youtube.com/watch?v=OhA5ayySXxw&list=PLCC34OHNcOtrZTjsC5qtn_X9eRAmGmGOI&index=8
+#
+# Filename:  		'Lesson-09 Strings.py'
+#
+#  $ cd "/c/Users/Nick/PycharmProjects/AA Python Programming 2021"      to get to the directory
+#
+#            Git push commands
+# git add .
+# git commit -am "SuitableText"
+# git push
+# <password, tavr>
+
+import os
+os.system("clear")
+
+print("Strings in Python - #9")
+
+greetings = 'My boss shouter \n"Get back to work"'  # use of quotes
+greetings = "My boss shouter \nGet back to work\"" # use of esq char \
+print(greetings)
+
+
+# Concationation only works with strings, no int, bool, etc
+name = "Steve Morse"
+greetings = "Hello, my name is " + name
+print(greetings)
+
+# Methods, function. Python is an oops laguage
+
+print(greetings.upper())
+print(greetings.lower())
+print(greetings.capitalize())
+print(greetings.title())
+print(greetings.swapcase())
+
+print(len(greetings))
+print(greetings[13])
+print(greetings[18:23])
+
+
+print(greetings.split(" "))		# Note the type changes from string to a list
+print(greetings.split(" ")[5])	# the 6th item in the list
+print(greetings.split(" ")[4:6])	# the 4th thru 6th item in the list
+
+
